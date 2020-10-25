078714903d88ef253f5fa00cc6a73e1771f45a23
yzhao <alexzhao@cs.utexas.edu>
2020-10-25 16:16:35 -0500

    Initial commit

--- detectron2/modeling/meta_arch/panoptic_match.py
+++ detectron2/modeling/meta_arch/panoptic_match.py
@@ -818,7 +818,7 @@ class HighResolutionNet(nn.Module):
         x_conf = self.last_layer_conf_conv(x)
         x_conf = torch.mean(x_conf,(2,3))
         x_conf = self.last_layer_conf_fc(x_conf)
-        x_conf = x_conf.view(x_conf.shape[0], self.num_instance, FOREGROUND_NUM)
+        x_conf = x_conf.view(x_conf.shape[0], self.num_instance, FOREGROUND_NUM+1)
 
         return x_sem, x_inst, x_conf
 
